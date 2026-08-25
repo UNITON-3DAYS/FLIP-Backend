8317d57292ddf5cfd3921a62b6c245233b501197
yueunfive <122276414+yueunfive@users.noreply.github.com>
2026-08-26 00:10:12 +0900

    refactor: WrongAnswerResponse 내 page 추가

--- src/main/kotlin/com/flip/domain/grading/presentation/WrongAnswerResponse.kt
+++ src/main/kotlin/com/flip/domain/grading/presentation/WrongAnswerResponse.kt
@@ -4,6 +4,9 @@ import com.flip.domain.grading.domain.GradingResult
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class WrongAnswerResponse(
+    @field:Schema(description = "페이지 번호", example = "12")
+    val page: String,
+
     @field:Schema(description = "문항 번호", example = "3")
     val questionNumber: String,
 
@@ -12,6 +15,7 @@ data class WrongAnswerResponse(
 ) {
     companion object {
         fun from(gradingResult: GradingResult) = WrongAnswerResponse(
+            page = gradingResult.page,
             questionNumber = gradingResult.questionNumber,
             studentAnswer = gradingResult.studentAnswer
         )
