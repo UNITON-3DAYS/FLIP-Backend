546c10d8e662ac114460c90bbfcff6182db58327
yueunfive <122276414+yueunfive@users.noreply.github.com>
2026-08-25 20:00:41 +0900

    refactor: /api 추가

--- src/main/kotlin/com/flip/domain/grading/presentation/GradingRecordController.kt
+++ src/main/kotlin/com/flip/domain/grading/presentation/GradingRecordController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/grading-records")
+@RequestMapping("/api/grading-records")
 class GradingRecordController(
     private val gradingRecordService: GradingRecordService
 ) {
