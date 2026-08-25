c5ab37e389d314c79fff2ce76403910e994e73ae
yueunfive <122276414+yueunfive@users.noreply.github.com>
2026-08-25 22:46:26 +0900

    fix: JsonProperty 추가

--- src/main/kotlin/com/flip/integration/ocr/infrastructure/GradingOcrRequest.kt
+++ src/main/kotlin/com/flip/integration/ocr/infrastructure/GradingOcrRequest.kt
@@ -3,8 +3,12 @@ package com.flip.integration.ocr.infrastructure
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class GradingOcrRequest(
+    @JsonProperty("worksheet_source")
     val workSheetSource: String,
+
+    @JsonProperty("name")
     val name: String,
+
     @JsonProperty("image_base64")
     val imageBase64: String
 )
